861649b27da96227f61fb4f3608283bb9f9c3a78
tonho911 <148846306+tonho911@users.noreply.github.com>
2024-11-30 18:57:53 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -188,7 +188,7 @@ rebuild_firmware() {
         base-files busybox ca-bundle -dnsmasq dnsmasq-full dropbear e2fsprogs firewall4 fstools kmod-gpio-button-hotplug kmod-nft-offload libc libgcc \
         logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only opkg partx-utils ppp ppp-mod-pppoe procd procd-seccomp procd-ujail uboot-envtools uci uclient-fetch urandom-seed urngd luci \
 	btrfs-progs zram-swap tar bash curl bind-tools ca-certificates luci-compat coreutils-nohup luci-mod-admin-full perlbase-file perlbase-time \
-        kmod-usb-net-asix-ax88179 \
+        kmod-usb-net-asix-ax88179 block-mount kmod-fs-ext4 fdisk kmod-usb-storage wget libmbedtls12 ip6tables-mod-nat \
         luci-app-amlogic \
         \
         ${config_list} \
